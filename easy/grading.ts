a1c88290cc2a03d5e304017bc6dfabc6d1c52ed5
Park Kiwook <fender.wook1002@gmail.com>
2021-12-07 08:59:55 +0900

    feat: implement grading

---
+++ easy/grading.ts
@@ -0,0 +1,13 @@
+function gradingStudents(grades: number[]): number[] {
+  // Write your code here
+  return grades.map((grade) => {
+    if (grade < 35) {
+      return grade;
+    }
+    if (5 - (grade % 5) >= 3) {
+      return grade;
+    } else if (5 - (grade % 5) < 3) {
+      return grade + 5 - (grade % 5);
+    }
+  });
+}
